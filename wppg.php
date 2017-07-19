d53a3cb2b23a6d7bd9755dd2b1f27cb3e74cc944
agus nurwanto <agusnurwantomuslim@gmail.com>
2017-07-19 12:30:19 +0700

    add plugin name

---
+++ wppg.php
@@ -0,0 +1,11 @@
+<?php
+/*
+  Plugin Name: WPPG
+  Plugin URI: http://qodr.or.id/
+  Version: 1.0.0
+  Author: Qodr Team
+  Author URI: http://qodr.or.id/
+  Description: Wordpress using postgressql
+  Text Domain: wppg
+  License: GPL
+ */
