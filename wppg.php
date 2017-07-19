c03cb805b8c311f22b4bc7d8861e0630b72950ae
ariefulkhakim <ariefulkhakim24@gmail.com>
2017-07-19 14:41:45 +0700

    buat sub menu wppg di menu settings dan buat tombol change database to postgressql

--- wppg.php
+++ wppg.php
@@ -13,5 +13,18 @@
 
 defined( 'ABSPATH' ) or die();
 
+add_action( 'admin_menu', 'wppgplugin_admin_options' );
+function wppgplugin_admin_options() {
+	add_options_page( 'Myplugin', 'WPPG', 'manage_options', 'wppg.php', 'wppgplugin_admin_submenu' );
+}
+
+function wppgplugin_admin_submenu() {
+	?>
+		<form>
+			<input type="submit" name="submit" class="btn-primary" value="submit">	
+		</form>
+	<?php
+}
+
 // - buat sub menu wppg di menu settings
 // - buat tombol change database to postgressql 
