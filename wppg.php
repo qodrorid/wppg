eea9e2386fb7e5b6b92a7c80429b21009149a017
agusnurwanto <agusnurwantomuslim@gmail.com>
2017-10-17 10:36:43 +0700

    delete directory

--- wppg.php
+++ wppg.php
@@ -20,18 +20,25 @@ function wppgplugin_admin_options() {
 
 function wppgplugin_admin_submenu() {
 	if (!empty($_POST)){
-		if(!empty($_POST['pg_action']) && $_POST['pg_action']=='move_pg4wp'){
-			$src = plugin_dir_path( __FILE__ ).'pg4wp';
-			$dst = explode('plugins', $src);
-			$dst = $dst[0].'pg4wp';
-			echo "Copy folder from $src to $dst <br>";
-			recurse_copy($src, $dst);
+		$src_pg4wp = plugin_dir_path( __FILE__ ).'pg4wp';
+		$dst = explode('plugins', $src_pg4wp);
+		$dst_pg4wp = $dst[0].'pg4wp';
 
-			$src = plugin_dir_path( __FILE__ ).'pg4wp/db.php';
-			$dst = explode('plugins', $src);
-			$dst = $dst[0].'db.php';
-			echo "Copy file from $src to $dst <br>";
-			copy($src, $dst);
+		$src_db = plugin_dir_path( __FILE__ ).'pg4wp/db.php';
+		$dst = explode('plugins', $src_db);
+		$dst_db = $dst[0].'db.php';
+		if(!empty($_POST['pg_action']) && $_POST['pg_action']=='move_pg4wp'){
+			echo "Copy folder from $src_pg4wp to $dst_pg4wp <br>";
+			recurse_copy($src_pg4wp, $dst_pg4wp);
+
+			echo "Copy file from $src_db to $dst_db <br>";
+			copy($src_db, $dst_db);
+		}else if(!empty($_POST['pg_action']) && $_POST['pg_action']=='move_mysql'){
+			echo "Delete folder $dst_pg4wp <br>";
+			deleteDir($dst_pg4wp);
+
+			echo "Delete file $dst_db <br>";
+			unlink($dst_db);
 		}
 	}
 	?> 	
@@ -42,15 +49,27 @@ function wppgplugin_admin_submenu() {
 		</style>
 		<form method="post" id="wppg-from">
 			<h1>WPPG</h1>
-			<p>Click this button for install this wordpress using postgressql database.</p>
-			<input type="hidden" name="pg_action" value="move_pg4wp">
-			<input type="submit" name="submit" class="button button-primary" value="change database to postgressql">	
+			<?php
+				if(defined('DB_DRIVER')){
+				    if(DB_DRIVER == 'pgsql'){
+			?>
+				    	<p>This wordpress is currently using Postgressql database.</p>
+						<input type="hidden" name="pg_action" value="move_mysql">
+						<input onclick="return confirm('Are you realy to using MSQL/Mariadb?');" type="submit" name="submit" class="button button-primary" value="change database to MYSQL/Mariadb">
+			<?php
+				    }
+				}else{
+			?>
+					<p>Click this button for install this wordpress using Postgressql database.</p>
+					<input type="hidden" name="pg_action" value="move_pg4wp">
+					<input onclick="return confirm('Are you realy to using Postgressql?');" type="submit" name="submit" class="button button-primary" value="change database to Postgressql">
+			<?php } ?>
 		</form>
 	<?php
 }
 
 function recurse_copy($src,$dst) { 
-    $dir = opendir($src);// echo '123';
+    $dir = opendir($src);
     mkdir($dst); 
     while(false !== ( $file = readdir($dir)) ) { 
         if (( $file != '.' ) && ( $file != '..' )) { 
@@ -64,6 +83,24 @@ function recurse_copy($src,$dst) {
     } 
     closedir($dir); 
 } 
+
+function deleteDir($dirPath) {
+    if (! is_dir($dirPath)) {
+        throw new InvalidArgumentException("$dirPath must be a directory");
+    }
+    if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
+        $dirPath .= '/';
+    }
+    $files = glob($dirPath . '*', GLOB_MARK);
+    foreach ($files as $file) {
+        if (is_dir($file)) {
+            deleteDir($file);
+        } else {
+            unlink($file);
+        }
+    }
+    rmdir($dirPath);
+}
 
 // SUDAH DIKERJAKAN
 // - buat sub menu wppg di menu settings
